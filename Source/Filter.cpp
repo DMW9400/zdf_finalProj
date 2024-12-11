cfca213bcb86059ddc54711174aa7689d48f2f09
DM Welch <dmw9400@gmail.com.com>
2024-12-10 22:35:22 -0500

    about to 3 screen it

---
+++ Source/Filter.cpp
@@ -0,0 +1,120 @@
+/*
+  ==============================================================================
+
+    Filter.cpp
+    Created: 10 Dec 2024 10:24:28pm
+    Author:  David Matthew Welch
+
+  ==============================================================================
+*/
+
+#include "Filter.h"
+
+VAKorg35Filter::VAKorg35Filter() {}
+
+    /** reset members to initialized state */
+    bool VAKorg35Filter::reset(double _sampleRate)
+    {
+        sampleRate = _sampleRate;
+        halfSamplePeriod = 1.0 / (2.0 * sampleRate);
+
+        for (uint32_t i = 0; i < KORG_SUBFILTERS; i++)
+        {
+            lpfVAFilters[i].reset(_sampleRate);
+            hpfVAFilters[i].reset(_sampleRate);
+        }
+
+        output.clearData();
+        return true;
+    }
+
+    void VAKorg35Filter::setFilterParams(double _fc, double _Q)
+    {
+        // --- use mapping function for Q -> K
+        mapDoubleValue(_Q, 1.0, 0.707, KORG35_Q_SLOPE);
+
+        if (fc != _fc || coeffs.K != _Q)
+        {
+            fc = _fc;
+            coeffs.K = _Q;
+            update();
+        }
+    }
+
+    bool VAKorg35Filter::update()
+    {
+        coeffs.g = tan(kTwoPi*fc*halfSamplePeriod); // (2.0 / T)*tan(wd*T / 2.0);
+        coeffs.alpha = coeffs.g / (1.0 + coeffs.g);
+
+        // --- alpha0 same for LPF, HPF
+        coeffs.alpha0 = 1.0 / (1.0 - coeffs.K*coeffs.alpha + coeffs.K*coeffs.alpha*coeffs.alpha);
+
+        // --- three sync-tuned filters
+        for (uint32_t i = 0; i < KORG_SUBFILTERS; i++)
+        {
+            lpfVAFilters[i].setAlpha(coeffs.alpha);
+            hpfVAFilters[i].setAlpha(coeffs.alpha);
+        }
+
+        // --- set filter beta values
+        lpfVAFilters[FLT2].setBeta((coeffs.K * (1.0 - coeffs.alpha)) / (1.0 + coeffs.g));
+        lpfVAFilters[FLT3].setBeta(-1.0 / (1.0 + coeffs.g));
+
+        hpfVAFilters[FLT2].setBeta(-coeffs.alpha / (1.0 + coeffs.g));
+        hpfVAFilters[FLT3].setBeta(1.0 / (1.0 + coeffs.g));
+
+        return true;
+    }
+
+    FilterOutput* VAKorg35Filter::process(double xn)
+    {
+        // --- lowpass
+        // --- process input through LPF1
+        FilterOutput* tempOut;
+        tempOut = lpfVAFilters[FLT1].process(xn);
+
+        // --- form S35
+        double S35 = lpfVAFilters[FLT2].getFBOutput() + lpfVAFilters[FLT3].getFBOutput();
+
+        // --- calculate u
+        double u = coeffs.alpha0*(tempOut->filter[ANM_LPF1] + S35);
+
+        // --- feed it to LPF2
+        tempOut = lpfVAFilters[FLT2].process(u);
+
+        // --- output = LPF*K
+        output.filter[LPF2] = tempOut->filter[LPF1] * coeffs.K;
+        output.filter[ANM_LPF2] = tempOut->filter[ANM_LPF1] * coeffs.K;
+
+        // --- feed output to HPF, no need to gather it's output
+        lpfVAFilters[FLT3].process(output.filter[LPF2]);
+
+        // --- HIGHPASS:
+        // --- process input through HPF1
+        tempOut = hpfVAFilters[FLT1].process(xn);
+
+        // --- then: form feedback and feed forward values (read before write)
+        S35 = hpfVAFilters[FLT2].getFBOutput() + hpfVAFilters[FLT3].getFBOutput();
+
+        // --- calculate u
+        u = coeffs.alpha0*(tempOut->filter[HPF1] + S35);
+
+        //---  form HPF output
+        output.filter[HPF2] = coeffs.K*u;
+
+        // --- process through feedback path
+        tempOut = hpfVAFilters[FLT2].process(output.filter[HPF2]);
+
+        // --- continue to LPF, no need to gather it's output
+        hpfVAFilters[FLT3].process(tempOut->filter[HPF1]);
+
+        // auto-normalize
+        if (coeffs.K > 0)
+        {
+            output.filter[ANM_LPF2] *= 1.0 / coeffs.K;
+            output.filter[LPF2] *= 1.0 / coeffs.K;
+            output.filter[HPF2] *= 1.0 / coeffs.K;
+        }
+
+        return &output;
+    }
--- Source/Filter.h
+++ Source/Filter.h
@@ -9,3 +9,122 @@
 */
 
 #pragma once
+#include "Interfaces.h"
+#include <cstdint>
+
+const double GUI_Q_MIN = 1.0;
+const double GUI_Q_MAX = 10.0;
+const double KORG35_Q_SLOPE = (2.0 - 0.707) / (GUI_Q_MAX - GUI_Q_MIN);
+
+struct VA1Coeffs
+{
+    // --- filter coefficients
+    double alpha = 0.0;            ///< alpha is (wcT/2)
+    double beta = 1.0;            ///< beta value, not used
+};
+
+const int KORG_SUBFILTERS = 3;
+enum { FLT1, FLT2, FLT3, FLT4 };
+
+class VA1Filter : public IFilterBase
+    {
+    public:
+        // --- constructor/destructor
+        VA1Filter();
+        virtual ~VA1Filter() {}
+
+        // --- these match SynthModule names
+        virtual bool reset(double _sampleRate);
+        virtual bool update();
+        virtual FilterOutput* process(double xn);
+        virtual void setFilterParams(double _fc, double _Q);
+
+        // --- set coeffs directly, bypassing coeff calculation
+        void setAlpha(double _alpha) { coeffs.alpha = _alpha; }
+        void setBeta(double _beta) { coeffs.beta = _beta; }
+        void setCoeffs(VA1Coeffs& _coeffs) {
+            coeffs = _coeffs;
+        }
+
+        void copyCoeffs(VA1Filter& destination) {
+            destination.setCoeffs(coeffs);
+        }
+        
+        // --- added for MOOG & K35, need access to this output value, scaled by beta
+        double getFBOutput() { return coeffs.beta * sn; }
+
+    protected:
+        FilterOutput output;
+        double sampleRate = 44100.0;                ///< current sample rate
+        double halfSamplePeriod = 1.0;
+        double fc = 0.0;
+
+        // --- state storage
+        double sn = 0.0;                        ///< state variables
+
+        // --- filter coefficients
+        VA1Coeffs coeffs;
+    };
+
+class VAKorg35Filter : public IFilterBase
+    {
+    public:
+        // --- constructor/destructor
+        VAKorg35Filter();
+        virtual ~VAKorg35Filter() {}
+
+        // --- these match SynthModule names
+        virtual bool reset(double _sampleRate);
+        virtual bool update();
+        virtual FilterOutput* process(double xn);
+        virtual void setFilterParams(double _fc, double _Q);
+        
+        struct VAKorg35Coeffs
+        {
+            // --- filter coefficients
+            double K = 1.0;            ///< beta value, not used
+            double alpha = 0.0;            ///< alpha is (wcT/2)
+            double alpha0 = 1.0;            ///< beta value, not used
+            double g = 1.0;            ///< beta value, not used
+        };
+        // --- set coeffs directly, bypassing coeff calculation
+        void setCoeffs(VAKorg35Coeffs& _coeffs) {
+            coeffs = _coeffs;
+
+            // --- three sync-tuned filters
+            for (uint32_t i = 0; i < KORG_SUBFILTERS; i++)
+            {
+                lpfVAFilters[i].setAlpha(coeffs.alpha);
+                hpfVAFilters[i].setAlpha(coeffs.alpha);
+            }
+
+            // --- set filter beta values
+            double deno = 1.0 + coeffs.g;
+
+            lpfVAFilters[FLT2].setBeta((coeffs.K * (1.0 - coeffs.alpha)) / deno);
+            lpfVAFilters[FLT3].setBeta(-1.0 / deno);
+
+            hpfVAFilters[FLT2].setBeta(-coeffs.alpha / deno);
+            hpfVAFilters[FLT3].setBeta(1.0 / deno);
+        //    hpfVAFilters[FLT3].setBeta(lpfVAFilters[FLT3].getBeta);
+        }
+
+        void copyCoeffs(VAKorg35Filter& destination) {
+            destination.setCoeffs(coeffs);
+        }
+
+    protected:
+        FilterOutput output;
+        VA1Filter lpfVAFilters[KORG_SUBFILTERS];
+        VA1Filter hpfVAFilters[KORG_SUBFILTERS];
+        double sampleRate = 44100.0;                ///< current sample rate
+        double halfSamplePeriod = 1.0;
+        double fc = 0.0;
+
+        // --- filter coefficients
+        VAKorg35Coeffs coeffs;
+
+        //double K = 0.0;
+        //double alpha = 0.0;            ///< alpha is (wcT/2)
+        //double alpha0 = 0.0;        ///< input scalar, correct delay-free loop
+    };
